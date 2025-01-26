609c0b2e653c4021a0db37cb9ea24d867cbfe61f
jb-j-lee <jb.dev.myjb@gmail.com>
2025-01-26 21:35:44 +0900

    Update README.md
    * Change from POST method to GET method
    * Change from LinearLayoutManager to GridLayoutManager

--- app/src/main/java/com/myjb/dev/model/remote/api/ApiService.kt
+++ app/src/main/java/com/myjb/dev/model/remote/api/ApiService.kt
@@ -1,11 +1,11 @@
 package com.myjb.dev.model.remote.api
 
 import retrofit2.Response
-import retrofit2.http.POST
+import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
-    @POST("{locale}/photos/search/{query}/")
+    @GET("{locale}/photos/search/{query}/")
     suspend fun getPhotos(
         @Path(value = "locale") locale: String = "ko",
         @Path(value = "query") query: String = "magic",
